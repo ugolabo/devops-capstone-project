993e9ce5c19a932b2968b4016089b4a3e9118d91
Hugues <hsanterre1@gmail.com>
2023-11-09 20:11:03 -0500

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -183,7 +183,7 @@ class TestAccountService(TestCase):
         headers = {
             'X-Frame-Options': 'SAMEORIGIN',
             'X-XSS-Protection': '1; mode=block',
-            'X-XSS-Protection': None, # CHEAT
+            'X-XSS-Protection': None,  # CHEAT
             'X-Content-Type-Options': 'nosniff',
             'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
             'Referrer-Policy': 'strict-origin-when-cross-origin'
